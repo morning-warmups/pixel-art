4452a5689f1633c77a081acc0a81786b77564a54
Matt Swann <matt@bfa.industries>
2016-02-03 10:12:09 +1100

    added some js

---
+++ solution/pixart.js
@@ -0,0 +1,47 @@
+// The document is loaded!
+$(document).ready(function() {
+	console.log("Loaded, bro.");
+
+	// STEP 1
+	// Listen for a click on the set color button...
+	var $colorButton = $("#set-color");
+	$colorButton.on("click", changeColor);
+
+	// Get the value of the input and change the CSS of the brush color box to that color.
+	function changeColor() {
+
+
+
+	};
+
+	// STEP 2
+	// Listen for a keypress event on the input field.
+	// When that happens, call changeColor.
+	var $colorInput = $("#color-field");
+
+
+
+	});
+
+	// Write a function that will change the color of each pixel that is mousedover..
+	function addColor(color, pixel) {
+
+
+
+	}
+
+	// STEP 3
+	// Create our canvas of pixels...
+	function createCanvas() {
+		var numSquares = 3040;
+		for (var i = 0; i < numSquares; i++) {
+
+
+
+		  });
+		}
+	}
+
+	createCanvas();
+
+});
